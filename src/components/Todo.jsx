84e0d5ab6515fd2bc02b74224ca87daf6e72b561
Kenn <markkenmadrid05@gmail.com>
2025-02-14 15:05:45 +0800

    change color

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react";
 import Form from "./Form";
 import Todolist from "./Todolist";
+import Footer from "./Footer";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.done).length;
+  const totalTodos = todos.length;
   return (
     <div>
       <Form todos={todos} setTodos={setTodos} />
-      <Todolist todos={todos} setTodos={setTodos}/>
+      <Todolist todos={todos} setTodos={setTodos} />
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos} />
     </div>
   );
 }
--- src/components/Todolist.jsx
+++ src/components/Todolist.jsx
@@ -1,9 +1,10 @@
 import styles from "./todolist.module.css";
 import TodoItem from "./TodoItem";
 export default function Todolist({ todos, setTodos }) {
+  const sortedTodos = todos.slice().sort((a,b)=>Number(a.done)-Number(b.done))
   return (
     <div className={styles.list}>
-      {todos.map((item) => (
+      {sortedTodos.map((item) => (
         <TodoItem
           key={item.name}
           item={item}
